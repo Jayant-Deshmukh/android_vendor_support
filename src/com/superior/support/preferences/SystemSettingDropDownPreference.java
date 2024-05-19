VoltageOS/packages_apps_Powerhub

Add SystemSettingDropDownPreference from lineageos
Change-Id: I71998d4d814ea982c85b91a52ff0a11ca473e92d

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/power/hub/preferences/SystemSettingDropDownPreference.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.power.hub.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import android.provider.Settings;
+
+
+public class SystemSettingDropDownPreference extends SelfRemovingDropDownPreference {
+    public SystemSettingDropDownPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SystemSettingDropDownPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public int getIntValue(int defValue) {
+        return getValue() == null ? defValue : Integer.valueOf(getValue());
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+
+    @Override
+    protected void putString(String key, String value) {
+        Settings.System.putString(getContext().getContentResolver(), key, value);
+    }
+
+    @Override
+    protected String getString(String key, String defaultValue) {
+        String s = Settings.System.getString(getContext().getContentResolver(),
+                key);
+         return s != null ? s : defaultValue;
+    }
+}

Jayant-Deshmukh/android_vendor_support

src/com/superior/support/preferences/SystemSettingDropDownPreference.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.superior.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import android.provider.Settings;
+
+
+public class SystemSettingDropDownPreference extends SelfRemovingDropDownPreference {
+    public SystemSettingDropDownPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SystemSettingDropDownPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public int getIntValue(int defValue) {
+        return getValue() == null ? defValue : Integer.valueOf(getValue());
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+
+    @Override
+    protected void putString(String key, String value) {
+        Settings.System.putString(getContext().getContentResolver(), key, value);
+    }
+
+    @Override
+    protected String getString(String key, String defaultValue) {
+        String s = Settings.System.getString(getContext().getContentResolver(),
+                key);
+         return s != null ? s : defaultValue;
+    }
+}